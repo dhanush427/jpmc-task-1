e7e2c196757aed08ec079c7cb2799c830da96bcc
Dhanush Ananthkar <dhanush427@gmail.com>
2023-03-24 00:57:12 -0400

    done

--- client.py
+++ client.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
 
         """ ----------- Update to get the ratio --------------- """
         prices = {}
+
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             prices[stock] = price
